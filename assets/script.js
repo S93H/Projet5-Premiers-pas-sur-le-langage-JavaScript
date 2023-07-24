30262e0de0bc411c17b93ff72cef9fe86af5de05
Sylvain <sylvain.hutt@epitech.eu>
2023-07-24 16:46:28 +0200

    Rendu sans commentaires

--- assets/script.js
+++ assets/script.js
@@ -22,7 +22,6 @@ const slides = [
 let arrow_left = document.getElementsByClassName('arrow_left')[0];
 let arrow_right = document.getElementsByClassName('arrow_right')[0];
 let indicatorParents = document.querySelector('.dots');
-// let p = document.querySelector('#banner p');
 
 
 
@@ -34,26 +33,6 @@ const tagLine = slides.map(slides => slides.tagLine)
 console.log(tagLine);
 
 let i = 0;
-
-
-
-
-//--------------------------------Fonctionne !!!!!! --------------------------------------//
-// function ChangeSlide(sens) {
-// 	i = i + sens;
-// 	if (i < 0) {
-// 		i = images.length - 1;
-// 	}
-// 	if (i > images.length -1) {
-// 		i = 0;
-// 	}
-
-// 	document.getElementsByClassName('banner-img')[0].src = `./assets/images/slideshow/${images[i]}`;
-// }
-//----------------------------------------------------------------------------------------//
-
-
-
 
 function ChangeSlide(sens) {
 	 	i = i + sens;
@@ -80,55 +59,6 @@ function ChangeSlide(sens) {
 
 
 
-// function ChangeTag(sens) {
-// 	i = i + sens;
-// 	if (i < 0) {
-// 		i = tagLine.length - 1;
-// 	}
-// 	if (i > tagLine.length -1) {
-// 		i = 0;
-// 	}
-
-// 	p.innerHTML=`${tagLine[i]}`;
-// }
-
-
-
-
-//---------------Fonctionnel--------------------------------------------------------------//
-
-
-// const slide = ["./assets/images/slideshow/slide1.jpg", "./assets/images/slideshow/slide2.jpg", "./assets/images/slideshow/slide3.jpg", "./assets/images/slideshow/slide4.png"];
-// let i = 0;
-
-// function ChangeSlide(sens) {
-// 	i = i + sens;
-// 	if (i < 0) {
-// 		i = slide.length - 1;
-// 	}
-// 	if (i > slide.length -1) {
-// 		i = 0;
-// 	}
-
-// 	document.getElementsByClassName('banner-img')[0].src = slide[i];
-// }
-
-
-//--------------------------------------------------------------------------------------------------//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 arrow_left.addEventListener('click', function () {
 	// console.log("click gauche !")
@@ -139,25 +69,12 @@ arrow_left.addEventListener('click', function () {
 	
 });
 
-// arrow_right.addEventListener('click', function () {
-// 	// console.log("click droit !")
-// 	document.getElementsByClassName('banner-img')[0].src="./assets/images/slideshow/slide2.jpg"
-	
-
-// });
 
 arrow_right.addEventListener('click', function () {
 	// console.log("click droit !")
-	// document.getElementsByClassName('banner-img')[0].src="./assets/images/slideshow/slide2.jpg"
 	ChangeSlide(1);
 	document.querySelector('.dots .dot_selected').classList.remove('dot_selected');
 	indicatorParents.children[i].classList.add('dot_selected');
 	
 
 });
-
-
-// arrow.addEventListener("click", () => {
-// 	console.log('coucou')
-// });
-
